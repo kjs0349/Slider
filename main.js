a1c116e7fda9b0b36e538a237ac4c4e5b39845a5
Jisu <kjl0349@naver.com>
2021-02-19 02:17:19 +0900

    <css> 이미지 사이즈 조절 및 transition 설정

--- main.js
+++ main.js
@@ -8,19 +8,16 @@ const btn2 = document.querySelector('.btn2');
 const btn3 = document.querySelector('.btn3');
 
 btn1.addEventListener('click', function() {
-    console.log(slider1);
     slider1.style.width = '100vw';
     slider2.style.width = '0';
     slider3.style.width = '0';
 });
 btn2.addEventListener('click', function() {
-    console.log(slider1);
     slider1.style.width = '0';
     slider2.style.width = '100vw';
     slider3.style.width = '0';
 });
 btn3.addEventListener('click', function() {
-    console.log(slider1);
     slider1.style.width = '0';
     slider2.style.width = '0';
     slider3.style.width = '100vw';
